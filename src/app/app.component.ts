abca545cc8cbba60570c48465247f278fe572207
“manzurkds” <manzurkds@gmail.com>
2019-07-11 17:27:20 +0530

    style: Cm checkbox styled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   items: any[];
   title = 'cm-select-app';
-  showCheckbox: boolean;
+  showCheckbox = true;
   selected: any[];
   selectedIds: number[];
 
